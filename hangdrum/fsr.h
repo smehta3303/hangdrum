ef63145eec2e8b4ee8fb7cb9275f64cf93f7c8de
smehta3303 <smehta3303@gmail.com>
2020-03-29 13:02:18 -0400

    Get max value for FSRs. Mild cleanup

--- hangdrum/fsr.h
+++ hangdrum/fsr.h
@@ -9,29 +9,29 @@
 #ifndef INCLUDE_FSR_H_
 #define INCLUDE_FSR_H_
 
-class FSR
+class Fsr
 {
 public:
-    FSR(uint32_t input_pin, uint32_t power_pin);
+    Fsr(uint32_t input_pin, uint32_t power_pin);
+    virtual ~Fsr() = default;
 
     /// @brief - Reads value from the ADC 
     /// @returns - measured FSR resistance
-    uint32_t readValue() const;
+    uint32_t read_value() const;
 
     /// @brief - Filters value using an averaging filter
     /// @returns - measured and filtered FSR resistance
-    uint32_t readFilteredValue() const;
+    uint32_t read_filtered_value() const;
 
     /// @brief - Scales value to MIDI velocity
     /// @returns - Midi velocity (0-127)
-    uint32_t scaleToMidi(uint32_t value) const;
+    uint32_t scale_to_midi(uint32_t value) const;
     
 private:
     uint32_t input_pin_;    // ADC pin
     uint32_t power_pin_;    // pin to use for 5V
     
     static constexpr size_t FILTER_SIZE = 3;    // Sample size of filter. Increaseing this would increase latency  
-    static constexpr uint32_t ADC_SCALE = 90;   // in percent
 };
 
 #endif
